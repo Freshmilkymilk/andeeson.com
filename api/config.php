a53559d53dfe75328db365cfe42bd536f777c54b
Michael Anderson <yadoneson175@gmail.com>
2016-12-08 15:37:11 -0700

    Initial

---
+++ api/config.php
@@ -0,0 +1,12 @@
+<?php
+    /**
+     * Your Twitter App Info
+     */
+
+    // Consumer Key
+    define('CONSUMER_KEY', 'Z4HBuA9DErvyM18N1uTvcCvya');
+    define('CONSUMER_SECRET', 'lePqzv5fyr7p4vZibyKegHBF5Fw0UovyS8EU0mS3GxaDSPuySi');
+
+    // User Access Token
+    define('ACCESS_TOKEN', '700479763840798721-8dCguPxhbIkq4XhqiGW4udk8cO1mNSi');
+    define('ACCESS_SECRET', 'uhfnBTjJcCTAhR4zFMHzaJqgo8hcihaHdKIzS4ThPLjK8');
